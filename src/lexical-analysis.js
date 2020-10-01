bd861117ea414a18ccb3138fb8620fb23954ce8c
letovDmitry <67585149+letovDmitry@users.noreply.github.com>
2020-10-01 22:38:00 +0600

    Added parser function

--- src/lexical-analysis.js
+++ src/lexical-analysis.js
@@ -2,17 +2,25 @@ function divideTokens(inputString) {  // TODO: add second argument to be an 'une
   const lexicalEntities = [];
   const pushToken = (type, value) => lexicalEntities.push({ type, value });
 
-  const detectOperator = (token) => /[+\-*\/\^%=(),]/.test(token);
+  const detectParenthesis = (token) => /\(|\)/.test(token);
+  const detectOperator = (token) => /[+\-*\/\^%=,]/.test(token);
   const detectDigit = (token) => /[0-9]/.test(token);
   const detectWhiteSpace = (token) => /\s/.test(token);
   const detectIdentifier = (token) => typeof token === 'string' && !detectOperator(token) && !detectDigit(token) && !detectWhiteSpace(token);
 
-  inputString.split(/\s+/).forEach((token) => {
-    if (detectOperator(token)) pushToken('operator', token);
-    if (detectDigit(token)) pushToken('digit', token);
-    if (detectIdentifier(token)) pushToken('identifier', token);
-    // else throw new Error('unexpected identifier');
+  inputString.replace(/\(/gm, ' ( ').replace(/\)/gm, ' )').split(/\s+/).forEach((token) => {
+    if (detectParenthesis(token)) pushToken('operator', token);
+    else if (detectOperator(token)) pushToken('operator', token);
+    else if (detectDigit(token)) pushToken('digit', token);
+    else if (detectWhiteSpace(token)) return;
+    else if (token === '') return;
+    else if (detectIdentifier(token)) pushToken('identifier', token);
+    else throw new Error('unexpected identifier');
   });
+
+  console.log(inputString.replace(/\(/gm, ' ( ').replace(/\)/gm, ' ) ').split(/\s+/))
 
   return lexicalEntities;
 }
+
+console.log(divideTokens('f(n) = 35'))
--- src/parser.js
+++ src/parser.js
@@ -11,5 +11,140 @@
 */ 
 
 function parse(tokens) {
+  const parseTree = [];
+  const symbols = {};
+  let i = 0;
+  
+  const bindingPower = (id, nud, lbp, led) => {
+    const symbol = symbols[id] || {};
+    symbols[id] = {
+      lbp: symbol.lbp || lbp,
+      nud: symbol.nud || nud,
+      led: symbol.led || led
+    }
+  }
 
+  const defineToken = (token) => {
+    const symbol = Object.create(symbols[token.type]);
+    symbol.type = token.type;
+    symbol.value = token.value;
+    return symbol;
+  }
+
+  const tokenize = () => {
+    return defineToken(tokens[i]);
+  }
+
+  const next = () => {
+    i++;
+    return tokenize();
+  }
+
+  const defineExpression = (rbp) => {
+    let left;
+    let Token = tokenize();
+
+    next();
+
+    if (!Token.nud) throw "Unexpected token: " + Token.type;
+    left = Token.nud(t);
+    while (rbp < token().lbp) {
+      Token = tokenize();
+      next();
+      if (!Token.led) throw "Unexpected token: " + Token.type;
+      left = Token.led(left);
+    }
+    return left;
+  }
+
+  const infixExpression = (id, lbp, rbp, led) => {
+    rbp = rbp || lbp;
+    bindingPower(id, null, lbp, led || function (left) {
+      return {
+        type: id,
+        left: left,
+        right: defineExpression(rbp)
+      };
+    });
+  }
+
+  const prefixExpression = (id, rbp) => {
+    bindingPower(id, function () {
+      return {
+        type: id,
+        right: defineExpression(rbp)
+      };
+    });
+  };
+
+  prefixExpression("-", 7);
+  infixExpression("^", 6, 5);
+  infixExpression("*", 4);
+  infixExpression("/", 4);
+  infixExpression("%", 4);
+  infixExpression("+", 3);
+  infixExpression("-", 3);
+
+  bindingPower(",");
+  bindingPower(")");
+  bindingPower("(end)");
+
+  bindingPower("(", () => {
+    value = defineExpression(2);
+    if (tokenize().type !== ")") throw "Expected closing parenthesis ')'";
+    next();
+    return value;
+  });
+  bindingPower("number", (number) => {
+      return number;
+  });
+
+  bindingPower("identifier", (name) => {
+    if (tokenize().type === "(") {
+      const args = [];
+      if (tokens[i + 1].type === ")") next();
+      else {
+        do {
+          next();
+          args.push(defineExpression(2));
+        } while (tokenize().type === ",");
+        if (tokenize().type !== ")") throw "Expected closing parenthesis ')'";
+      }
+      next();
+      return {
+        type: "call",
+        args: args,
+        name: name.value
+      };
+    }
+    return name;
+  });
+  infixExpression("=", 1, 2, (left) => {
+      if (left.type === "call") {
+        for (var i = 0; i < left.args.length; i++) {
+          if (left.args[i].type !== "identifier") throw "Invalid argument name";
+        }
+        return {
+          type: "function",
+          name: left.name,
+          args: left.args,
+          value: defineExpression(2)
+        };
+      } else if (left.type === "identifier") {
+        return {
+          type: "assign",
+          name: left.value,
+          value: defineExpression(2)
+        };
+      }
+      else throw "Invalid lvalue";
+  });
+  
+  while (tokenize().type !== "(end)") {
+    parseTree.push(expression(0));
+  }
+  
+  return parseTree;
 }
+
+console.log(parse());
